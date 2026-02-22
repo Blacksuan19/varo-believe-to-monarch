5cd77fad4e298511d1f53516fb21fb9f431a3a29
blacksuan19 <abubakaryagob@gmail.com>
2026-02-22 14:00:42 -0600

    Bump version 0.4.8 -> 0.4.9

--- varo_to_monarch/__init__.py
+++ varo_to_monarch/__init__.py
@@ -4,7 +4,7 @@ from .cli import app, convert
 from .extractors import extract_transactions_from_pdf
 from .processing import finalize_monarch
 
-__version__ = "0.4.8"
+__version__ = "0.4.9"
 __all__ = [
     "app",
     "convert",
